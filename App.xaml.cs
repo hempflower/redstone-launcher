7e83cac5a27b9ef2f803aefd143773f720097cc2
hempflower <1993996310@qq.com>
2020-01-13 18:32:58 +0800

    初始提交

---
+++ App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using RedstoneLauncher.tools;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace RedstoneLauncher
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+        private static Logger logger { get; } = new Logger();
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            Console.WriteLine("Redstone Launcher 1.0.0");
+        }
+        public static Logger getLogger()
+        {
+            return logger;
+        }
+        
+    }
+}
---
+++ tools/Logger.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedstoneLauncher.tools
+{
+    /// <summary>
+    /// 日志类
+    /// </summary>
+    public class Logger
+    {
+        private StringBuilder logBuffer = new StringBuilder();
+
+        /// <summary>
+        /// 写入行
+        /// </summary>
+        /// <param name="content">欲写入的内容</param>
+        public void WriteLine(String content)
+        {
+            Console.WriteLine(content);
+            logBuffer.AppendLine(content);
+        }
+        /// <summary>
+        /// INFO
+        /// </summary>
+        /// <param name="content">欲写入的内容</param>
+        /// <param name="srcPath"></param>
+        /// <param name="line"></param>
+        /// <param name="member"></param>
+        public void Info(String content, [CallerFilePath] String srcPath = "(Unknown)", [CallerLineNumber] int line = 0, [CallerMemberName] string member = "(Unknown)")
+        {
+            var type = "INFO";
+            this.WriteLine(String.Format("[{0}][{1}][{2}] {3}", DateTime.Now.ToString(), Path.GetFileName(srcPath) + ":" + line + "@" + member, type, content));
+        }
+
+        /// <summary>
+        /// WARN
+        /// </summary>
+        /// <param name="content">欲写入的内容</param>
+        /// <param name="srcPath"></param>
+        /// <param name="line"></param>
+        /// <param name="member"></param>
+        public void Warn(String content, [CallerFilePath] String srcPath = "(Unknown)", [CallerLineNumber] int line = 0, [CallerMemberName] string member = "(Unknown)")
+        {
+            var type = "WARN";
+            this.WriteLine(String.Format("[{0}][{1}][{2}] {3}", DateTime.Now.ToString(), Path.GetFileName(srcPath) + ":" + line + "@" + member, type, content));
+        }
+
+        /// <summary>
+        /// ERROR
+        /// </summary>
+        /// <param name="content">欲写入的内容</param>
+        /// <param name="srcPath"></param>
+        /// <param name="line"></param>
+        /// <param name="member"></param>
+        public void Error(String content, [CallerFilePath] String srcPath = "(Unknown)", [CallerLineNumber] int line = 0, [CallerMemberName] string member = "(Unknown)")
+        {
+            var type = "ERROR";
+            this.WriteLine(String.Format("[{0}][{1}][{2}] {3}", DateTime.Now.ToString(), Path.GetFileName(srcPath) + ":" + line + "@" + member, type, content));
+        }
+
+        /// <summary>
+        /// DEBUG
+        /// </summary>
+        /// <param name="content">欲写入的内容</param>
+        /// <param name="srcPath"></param>
+        /// <param name="line"></param>
+        /// <param name="member"></param>
+        public void Debug(String content, [CallerFilePath] String srcPath = "(Unknown)", [CallerLineNumber] int line = 0, [CallerMemberName] string member = "(Unknown)")
+        {
+            var type = "DEBUG";
+            this.WriteLine(String.Format("[{0}][{1}][{2}] {3}", DateTime.Now.ToString(), Path.GetFileName(srcPath) + ":" + line + "@" + member, type, content));
+        }
+
+        /// <summary>
+        /// 将日志导出
+        /// </summary>
+        /// <returns>导出的日志</returns>
+        public String dump()
+        {
+            return logBuffer.ToString();
+        }
+    }
+}
